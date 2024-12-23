5e1fc906c562f09812ec313fb110cca98e5e17f8
Alex Cristoffanini <acristoffanini@gmail.com>
2024-12-22 17:12:36 -0800

    control update

--- game/characters/base_player.gd
+++ game/characters/base_player.gd
@@ -42,11 +42,11 @@ func _physics_process(delta: float) -> void:
 		velocity += get_gravity() * delta
 
 	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+	if Input.is_action_just_pressed("jump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
 	#Change to controller compatible inputs when nicholas updates the controller-controller
-	var horizontalDirection = Input.get_axis("ui_left", "ui_right")
-	var verticalDirection = Input.get_axis("ui_up", "ui_down")
+	var horizontalDirection = Input.get_axis("move_left", "move_right")
+	#var verticalDirection = Input.get_axis("ui_up", "ui_down")
 	#Flip the sprite whether moving left or right
 	if horizontalDirection:
 		sprite.flip_h = (horizontalDirection == -1)
