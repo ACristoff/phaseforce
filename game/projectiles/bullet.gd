006b765b8a1fbcf2dd8d8e93b112068379b81dd2
kinteyoi <calebazlin5@gmail.com>
2024-12-29 19:12:20 -0600

    bullet paerticles

--- game/projectiles/bullet.gd
+++ game/projectiles/bullet.gd
@@ -1,6 +1,8 @@
 extends Area2D
 
 @export var speed = 600
+@onready var PARTICLE = preload("res://assets/particles/hit_particles.tscn")
+
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -16,3 +18,18 @@ func _process(delta):
 
 func _on_area_entered(area):
 	pass # Replace with function body.
+	
+
+
+
+func _on_body_entered(body):
+	if body.has_method("metal"):
+		print("hit metal tile")
+		
+	elif body.has_method("snow"):
+		var particle = PARTICLE.instantiate()
+		get_tree().current_scene.add_child(particle)
+		#particle.global_position = $".".global_position
+		particle.type = 1
+		print("hit snow tile")
+		queue_free()
