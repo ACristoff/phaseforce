6d40a39435e6d8dabd4a7e03f1270b790b68c31f
Alex Cristoffanini <acristoffanini@gmail.com>
2024-12-30 17:46:35 -0800

    level and game manager planning

---
+++ game/data/level_manager.gd
@@ -0,0 +1,11 @@
+extends Node
+
+@export var levels: Array[PackedScene] = []
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+func load_level(id):
+	var new_level = levels[id].instantiate()
+	add_child(new_level)
