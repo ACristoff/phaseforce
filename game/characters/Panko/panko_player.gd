7c270f07c5de8c9156d20da507427cf85cfc4d46
Alex Cristoffanini <acristoffanini@gmail.com>
2024-12-31 11:51:43 -0800

    code cleanup

--- game/characters/Panko/panko_player.gd
+++ game/characters/Panko/panko_player.gd
@@ -7,14 +7,11 @@ func attack():
 	if !powered_up:
 		var knockback_vector = (cursor_spout.global_position - global_position) * knockback_force
 		if knockback_vector.y < 0:
-			#print('big jump')
 			velocity.y = knockback_vector.y * 4
 		else:
-			#print('small jump')
 			knockback_vector.y = knockback_vector.y - 40
 			if knockback_vector.y < 0:
 				velocity.y = knockback_vector.y * 4
 			velocity.y = knockback_vector.y - 30
 			
 		knockback = knockback_vector * 3
-		#prints('big knockback', knockback_vector)
