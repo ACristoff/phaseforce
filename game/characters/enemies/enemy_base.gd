27d230acdbebfca16a4f0bc5e9dd2b8b9573c41f
Alex Cristoffanini <acristoffanini@gmail.com>
2024-12-31 14:54:08 -0800

    enemy based

--- game/characters/enemies/enemy_base.gd
+++ game/characters/enemies/enemy_base.gd
@@ -11,13 +11,11 @@ var alert: bool = false
 func _ready():
 	pass # Replace with function body.
 
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	#Add the gravity.
-	#velocity += get_gravity() * delta
-	
-	pass
+func _physics_process(delta):
+	#Add the gravity
+	if not is_on_floor():
+		velocity += get_gravity() * delta
+	move_and_slide()
 
 func idle():
 	
@@ -30,3 +28,29 @@ func track_player():
 func attack():
 	
 	pass
+
+#extends CharacterBody2D
+#
+#
+#const SPEED = 300.0
+#const JUMP_VELOCITY = -400.0
+
+
+#func _physics_process(delta):
+	## Add the gravity.
+	#if not is_on_floor():
+		#velocity += get_gravity() * delta
+#
+	## Handle jump.
+	#if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+		#velocity.y = JUMP_VELOCITY
+#
+	## Get the input direction and handle the movement/deceleration.
+	## As good practice, you should replace UI actions with custom gameplay actions.
+	#var direction = Input.get_axis("ui_left", "ui_right")
+	#if direction:
+		#velocity.x = direction * SPEED
+	#else:
+		#velocity.x = move_toward(velocity.x, 0, SPEED)
+#
+	#move_and_slide()
