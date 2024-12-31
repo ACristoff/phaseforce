8501bd1e8feeafcb733bbc4897dcc0c1dee5549a
Alex Cristoffanini <acristoffanini@gmail.com>
2024-12-30 19:47:12 -0800

    programmatic spawning

--- game/world/level.gd
+++ game/world/level.gd
@@ -2,9 +2,13 @@ extends Node2D
 
 @export var music = preload("res://assets/music/PF_MAIN_THEME.mp3")
 
+@onready var spawn = $SpawnPoint
+
 var character: PackedScene
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	print(character)
+	var spawn_char = character.instantiate()
+	spawn_char.global_position = spawn.global_position
+	add_child(spawn_char)
 	pass # Replace with function body.
