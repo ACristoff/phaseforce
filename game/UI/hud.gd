eb83fe71585d495906bf9c5e5bd29c3ec173b8a5
kinteyoi <calebazlin5@gmail.com>
2025-01-01 13:58:33 -0600

    hud additiona

--- game/UI/hud.gd
+++ game/UI/hud.gd
@@ -8,9 +8,16 @@ extends Control
 @export var charge_cannon_ammo = preload("res://assets/ui/ammo_types/power_cell.png")
 @export var shotgun_ammo = preload("res://assets/ui/ammo_types/shotgun_shell.png")
 
+@onready var obj1 = $CanvasLayer/Objective_Container/VBoxContainer/HBoxContainer
+@onready var obj2 = $CanvasLayer/Objective_Container/VBoxContainer/HBoxContainer2
+@onready var obj3 = $CanvasLayer/Objective_Container/VBoxContainer/HBoxContainer3
+@onready var obj1label = $CanvasLayer/Objective_Container/VBoxContainer/HBoxContainer/Label
+@onready var obj2label = $CanvasLayer/Objective_Container/VBoxContainer/HBoxContainer2/Label
+@onready var obj3label = $CanvasLayer/Objective_Container/VBoxContainer/HBoxContainer3/Label
 
-
-
+var killed_snowmen = 0
+var amount_to_kill = 10
+var level = 1
 
 
 # Called when the node enters the scene tree for the first time.
@@ -20,4 +27,35 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	pass
+	if level == 1:
+		obj1label.text = "Destroy the Generator"
+		obj2label.text = "Kill  "+str(killed_snowmen)+"/"+str(amount_to_kill)+"  Snowmen"
+		if killed_snowmen == amount_to_kill:
+			completeobj2()
+		obj3label.text = "Escape"
+	
+func kill_snowman():
+	killed_snowmen += 1
+	
+func completeob1():
+	obj1.pivot_offset = obj1.size/2
+	var tween = create_tween()
+	tween.tween_property(obj1, "scale", Vector2(1.2, 1), .15)
+	tween.tween_property(obj1, "scale", Vector2(1, 1), .15)
+	obj1.modulate = Color.GREEN_YELLOW
+func completeobj2():
+	obj2.pivot_offset = obj1.size/2
+	var tween = create_tween()
+	tween.tween_property(obj2, "scale", Vector2(1.2, 1), .15)
+	tween.tween_property(obj2, "scale", Vector2(1, 1), .15)
+	obj2.modulate = Color.GREEN_YELLOW
+func completeobj3():
+	obj3.pivot_offset = obj1.size/2
+	var tween = create_tween()
+	tween.tween_property(obj3, "scale", Vector2(1.2, 1), .15)
+	tween.tween_property(obj3, "scale", Vector2(1, 1), .15)
+	obj3.modulate = Color.GREEN_YELLOW
+
+
+func _on_button_pressed():
+	completeob1()
