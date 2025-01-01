a0640a40c8214499f8f714e70cc4b194256cb951
kinteyoi <calebazlin5@gmail.com>
2024-12-31 20:11:30 -0600

    bullet hit particle

--- game/projectiles/bullet_hit_particle.gd
+++ game/projectiles/bullet_hit_particle.gd
@@ -1,12 +1,15 @@
 extends GPUParticles2D
-
+@export var type = 1
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	emitting = true
-	pass # Replace with function body.
+	if type == 1:
+		self.modulate = Color.WHITE
+	else: 
+		self.modulate = Color.YELLOW
 
-#
+
 ## Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
 	#pass
