7883c8a24d02f5d9a5333bd610fdd82338c45aa3
kinteyoi <calebazlin5@gmail.com>
2025-01-01 01:39:19 -0600

    hud

---
+++ game/UI/hud.gd
@@ -0,0 +1,23 @@
+extends Control
+
+@export_group("Ammo Types")
+@export var smg_ammo = preload("res://assets/ui/ammo_types/45cal.png")
+@export var deagle_ammo = preload("res://assets/ui/ammo_types/50cal.png")
+@export var rifle_ammo = preload("res://assets/ui/ammo_types/223remington.png")
+@export var artillery_ammo = preload("res://assets/ui/ammo_types/artillery_shell.png")
+@export var charge_cannon_ammo = preload("res://assets/ui/ammo_types/power_cell.png")
+@export var shotgun_ammo = preload("res://assets/ui/ammo_types/shotgun_shell.png")
+
+
+
+
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
