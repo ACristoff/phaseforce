28cea691c7463013ab78b6e210b3ff1e8979b65a
Alex Cristoffanini <acristoffanini@gmail.com>
2025-01-02 02:23:16 -0800

    record player stuff

---
+++ game/objects/record_player.gd
@@ -0,0 +1,9 @@
+extends StaticBody2D
+
+@export var music = preload("res://assets/music/Pippa the Ripper.mp3")
+
+
+func _on_area_2d_body_entered(body):
+	print('body entered record player': body)
+	if body is BasePlayer:
+		AudioManager.play_music(music)
