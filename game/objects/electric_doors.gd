727a74bfca17262dfa809acd8fb668f892ac6319
kinteyoi <calebazlin5@gmail.com>
2025-01-02 22:37:20 -0600

    electric door

---
+++ game/objects/electric_doors.gd
@@ -0,0 +1,18 @@
+extends AnimatableBody2D
+
+var interactable = true
+@onready var light_bulb = $LightBulb
+@onready var light = $LightBulb/PointLight2D
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	if interactable == true:
+		light_bulb.modulate = Color.GREEN
+		light.color = Color.GREEN
+	else:
+		light_bulb.modulate = Color.RED
+		light.color = Color.RED
