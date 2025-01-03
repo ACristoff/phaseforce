40d75846acb9485e22156a7b6f1772b91a41a005
Alex Cristoffanini <acristoffanini@gmail.com>
2025-01-03 02:31:29 -0800

    conditional extract

--- game/UI/hud.gd
+++ game/UI/hud.gd
@@ -14,10 +14,10 @@ class_name HUD
 
 #@onready var obj1 = $CanvasLayer/Objective_Container/VBoxContainer/HBoxContainer
 @onready var obj2 = $CanvasLayer/Objective_Container/VBoxContainer/HBoxContainer2
-@onready var obj3 = $CanvasLayer/Objective_Container/VBoxContainer/HBoxContainer3
+#@onready var obj3 = $CanvasLayer/Objective_Container/VBoxContainer/HBoxContainer3
 #@onready var obj1label = $CanvasLayer/Objective_Container/VBoxContainer/HBoxContainer/Label
 @onready var obj2label = $CanvasLayer/Objective_Container/VBoxContainer/HBoxContainer2/Label
-@onready var obj3label = $CanvasLayer/Objective_Container/VBoxContainer/HBoxContainer3/Label
+#@onready var obj3label = $CanvasLayer/Objective_Container/VBoxContainer/HBoxContainer3/Label
 
 @onready var primary_obj = $CanvasLayer/Objective_Container/VBoxContainer/PrimaryObjectiveLabel
 @onready var primary_obj_label = $CanvasLayer/Objective_Container/VBoxContainer/PrimaryObjectiveLabel/Label
@@ -30,6 +30,10 @@ var level = 1
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	if level == 1:
+		#obj1label.text = "Destroy the Generator"
+		obj2label.text = "Kill  "+str(killed_snowmen)+"/"+str(amount_to_kill)+"  Snowmen"
+		#obj3label.text = "Escape"
 	pass # Replace with function body.
 
 func take_damage():
@@ -40,10 +44,8 @@ func update_primary():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	if level == 1:
-		#obj1label.text = "Destroy the Generator"
-		obj2label.text = "Kill  "+str(killed_snowmen)+"/"+str(amount_to_kill)+"  Snowmen"
-		obj3label.text = "Escape"
+	#if p
+	pass
 	
 func killed_snowman():
 	obj2.pivot_offset = obj2.size/2
@@ -60,7 +62,11 @@ func complete_primary():
 	tween.tween_property(primary_obj, "scale", Vector2(1.2, 1), .15)
 	tween.tween_property(primary_obj, "scale", Vector2(1, 1), .15)
 	primary_obj.modulate = Color.GREEN_YELLOW
+	go_to_extract()
 
+func go_to_extract():
+	extract_obj.visible = true
+	#pass
 
 func completeobj2():
 	obj2.pivot_offset = obj2.size/2
@@ -68,12 +74,14 @@ func completeobj2():
 	tween.tween_property(obj2, "scale", Vector2(1.2, 1), .15)
 	tween.tween_property(obj2, "scale", Vector2(1, 1), .15)
 	obj2.modulate = Color.GREEN_YELLOW
+
 func completeobj3():
-	obj3.pivot_offset = obj3.size/2
-	var tween = create_tween()
-	tween.tween_property(obj3, "scale", Vector2(1.2, 1), .15)
-	tween.tween_property(obj3, "scale", Vector2(1, 1), .15)
-	obj3.modulate = Color.GREEN_YELLOW
+	#obj3.pivot_offset = obj3.size/2
+	#var tween = create_tween()
+	#tween.tween_property(obj3, "scale", Vector2(1.2, 1), .15)
+	#tween.tween_property(obj3, "scale", Vector2(1, 1), .15)
+	#obj3.modulate = Color.GREEN_YELLOW
+	pass
 
 
 func _on_button_pressed():
