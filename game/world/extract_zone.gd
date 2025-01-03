540e2c17925428337b94c31eb8012cd7cbb4a912
Alex Cristoffanini <acristoffanini@gmail.com>
2025-01-02 19:27:45 -0800

    quip testing

--- game/world/extract_zone.gd
+++ game/world/extract_zone.gd
@@ -1,15 +1,8 @@
 extends Node2D
 
-
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
-
+signal player_extracted
 
 func _on_area_2d_body_entered(body):
+	if body is BasePlayer:
+		pass
 	pass # Replace with function body.
