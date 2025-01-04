755af59cb5d34153658798675e3eccb7b70c34f5
Alex Cristoffanini <acristoffanini@gmail.com>
2025-01-04 03:59:05 -0800

    pippa knockback setup

--- game/characters/pippa/pippa_player.gd
+++ game/characters/pippa/pippa_player.gd
@@ -1,1 +1,29 @@
 extends BasePlayer
+
+
+var knockback_force = -2
+@onready var TRANSEFFECT = preload("res://game/effects/transformation_sequence.tscn")
+
+func attack():
+	super()
+	if !powered_up:
+		var knockback_vector = (cursor_spout.global_position - global_position) * knockback_force
+		#velocity.y = knockback_vector.y 
+		#if knockback_vector.y < 0:
+			#
+		#else:
+			#knockback_vector.y = knockback_vector.y - 40
+			#if knockback_vector.y < 0:
+				#velocity.y = knockback_vector.y 
+			#velocity.y = knockback_vector.y - 30
+		knockback = knockback_vector * 3
+	else:
+		pass
+
+func power_up():
+	super()
+	var trans = TRANSEFFECT.instantiate()
+	self.add_child(trans)
+	trans.type = 1
+	trans.begin()
+	trans.global_position = self.global_position
