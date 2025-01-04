e4d1bf64b8ea142d467b423ad774841ce377eba0
kinteyoi <calebazlin5@gmail.com>
2025-01-04 03:41:56 -0600

    choppa

--- game/UI/inside_heli.gd
+++ game/UI/inside_heli.gd
@@ -1,12 +1,13 @@
 extends Node2D
 
 var shake_amount = .1
+@onready var graphic = $Insideheli
+@onready var choppa = preload("res://assets/sfx/objects/CHOPPA_CLOSE.mp3")
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	AudioManager.play_music(choppa)
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	func _process(delta):
 	graphic.offset = Vector2(randi_range(-1, 1) * shake_amount, randi_range(-1, 1) * shake_amount)
