b684728ef0a9590ba7e8c26f8f2751ad48826c59
Alex Cristoffanini <acristoffanini@gmail.com>
2025-01-04 04:04:56 -0800

    pippa powered knockback

--- game/characters/pippa/pippa_player.gd
+++ game/characters/pippa/pippa_player.gd
@@ -1,24 +1,25 @@
 extends BasePlayer
 
 
-var knockback_force = -2
+var knockback_force = -1
+var powered_knockback_force = -5
 @onready var TRANSEFFECT = preload("res://game/effects/transformation_sequence.tscn")
 
 func attack():
 	super()
 	if !powered_up:
 		var knockback_vector = (cursor_spout.global_position - global_position) * knockback_force
-		#velocity.y = knockback_vector.y 
-		#if knockback_vector.y < 0:
-			#
-		#else:
-			#knockback_vector.y = knockback_vector.y - 40
-			#if knockback_vector.y < 0:
-				#velocity.y = knockback_vector.y 
-			#velocity.y = knockback_vector.y - 30
-		knockback = knockback_vector * 3
+		knockback = knockback_vector * 2
 	else:
-		pass
+		var knockback_vector = (cursor_spout.global_position - global_position) * powered_knockback_force
+		if knockback_vector.y < 0:
+			velocity.y = knockback_vector.y * 4
+		else:
+			knockback_vector.y = knockback_vector.y - 40
+			if knockback_vector.y < 0:
+				velocity.y = knockback_vector.y * 4
+			velocity.y = knockback_vector.y - 30
+		knockback = knockback_vector * 3
 
 func power_up():
 	super()
