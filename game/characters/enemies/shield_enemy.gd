4e9243603b1d0348c396148b25585c2b883dee93
Alex Cristoffanini <acristoffanini@gmail.com>
2025-01-04 17:32:14 -0800

    shield enemy v1

--- game/characters/enemies/shield_enemy.gd
+++ game/characters/enemies/shield_enemy.gd
@@ -4,8 +4,11 @@ class_name ShieldEnemy
 #@onready var shield_parent = $Shield
 @onready var shield_hitbox = $ShieldHitBox
 @onready var shield_collision = $ShieldHitBox/CollisionShape2D
-
+@onready var shieldless = preload("res://assets/enemies/enemysnowman3sheildless.png")
 @export var shield_health = 120
+@onready var shield_hit_sound = preload("res://assets/sfx/characters/SHIELD_HIT.mp3")
+@onready var shield_break_sound = preload("res://assets/sfx/misc/SHIELD_BREAK.mp3")
+
 
 func shield_take_damage(damage):
 	shield_health -= damage
@@ -15,6 +18,10 @@ func shield_take_damage(damage):
 		shield_collision.set_deferred("disabled", true)
 		shield_hitbox.monitoring = false
 		shield_hitbox.visible = false
+		sprite.texture = shieldless
+		AudioManager.play_sfx(shield_break_sound)
+	else:
+		AudioManager.play_sfx(shield_hit_sound)
 
 func turn(direction):
 	facing_right = direction
@@ -26,7 +33,8 @@ func turn(direction):
 		gun.scale.x = 1
 		gun_barrel.position.x = 19
 		shield_hitbox.scale.x = 1
-		sprite.scale.x = 1
+		collision.position.x = 0
+		#sprite.position.x = 0
 	else:
 		sprite.flip_h = true
 		floor_cast.position.x = -10
@@ -35,4 +43,5 @@ func turn(direction):
 		gun.scale.x = -1
 		gun_barrel.position.x = -17
 		shield_hitbox.scale.x = -1
-		sprite.scale.x = -1
+		collision.position.x = 5
+		#sprite.position.x = 5
