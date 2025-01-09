1c2d209a7df7cbf0315728ac60857259f402d76b
kinteyoi <calebazlin5@gmail.com>
2025-01-09 02:47:14 -0600

    game over

--- game/UI/menus/game_over.gd
+++ game/UI/menus/game_over.gd
@@ -1,12 +1,14 @@
 extends Node2D
 
 @onready var music = preload("res://assets/music/PF_DEFEAT.mp3")
+@onready var anim = $AnimationPlayer
 
 signal retry
 signal level_select
 signal quit_to_main
 
 func _ready():
+	anim.play("you_graduated")
 	AudioManager.play_music(music)
 
 func _on_button_pressed():
