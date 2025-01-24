cfaae735bc1a2f522f697100c4deef2ea38dfde2
kinteyoi <calebazlin5@gmail.com>
2025-01-24 13:03:03 -0600

    win con has end implementation

--- game/UI/menus/win_con.gd
+++ game/UI/menus/win_con.gd
@@ -9,6 +9,10 @@ extends Control
 @onready var heli = $InsideHeli
 @onready var select_anim = $CanvasLayer/SelectedButtonGraphic/AnimationPlayer
 @onready var select_graphic = $CanvasLayer/SelectedButtonGraphic
+@onready var retry_button = $CanvasLayer/MarginContainer/Control/HBoxContainer/Retry
+@onready var level_select_button = $CanvasLayer/MarginContainer/Control/HBoxContainer/NextLevel
+@onready var continue_button = $CanvasLayer/MarginContainer/Control/HBoxContainer/Continue
+@onready var main_menu_button = $CanvasLayer/MarginContainer/Control/MarginContainer/MainMenu
 
 #Characters
 @onready var panko = $PankoSprite
@@ -21,6 +25,8 @@ extends Control
 @onready var victory_music = preload("res://assets/music/VICTORY_A.mp3")
 @onready var victory_music_b = preload("res://assets/music/VICTORY_B.mp3")
 
+var all_levels_completed = false
+
 var game_man: game_manager
 var level_man: Level_Manager
 
@@ -32,24 +38,52 @@ func _star_three_achieved():
 	starthree.texture = purplestar
 
 func _ready():
-	#AudioManager.play_sfx(victory_music)
-	var new_song: AudioStreamPlayer = AudioManager.play_music(victory_music, 0, false)
-	AudioManager.music_manager.finished.connect(track_b.bind())
-	game_man = get_tree().get_first_node_in_group("game")
-	level_man = get_tree().get_first_node_in_group("level_manager")
-	print(game_man.current_character)
-	var character = game_man.current_character
-	if character == "panko":
-		panko.visible = true
-	if character == "lumi":
-		lumi.visible = true
-	if character == "pippa":
-		pippa.visible = true
-	if character == "tenma":
-		tenma.visible = true
-	if character == "lia":
-		lia.visible = true
-	#$Timer.start()
+	if all_levels_completed == false:
+		#AudioManager.play_sfx(victory_music)
+		var new_song: AudioStreamPlayer = AudioManager.play_music(victory_music, 0, false)
+		AudioManager.music_manager.finished.connect(track_b.bind())
+		game_man = get_tree().get_first_node_in_group("game")
+		level_man = get_tree().get_first_node_in_group("level_manager")
+		print(game_man.current_character)
+		var character = game_man.current_character
+		if character == "panko":
+			panko.visible = true
+		if character == "lumi":
+			lumi.visible = true
+		if character == "pippa":
+			pippa.visible = true
+		if character == "tenma":
+			tenma.visible = true
+		if character == "lia":
+			lia.visible = true
+		retry_button.visible = true
+		level_select_button.visible = true
+		main_menu_button.visible = true
+		continue_button.visible = false
+		#$Timer.start()
+	else:
+		#AudioManager.play_sfx(victory_music)
+		var new_song: AudioStreamPlayer = AudioManager.play_music(victory_music, 0, false)
+		AudioManager.music_manager.finished.connect(track_b.bind())
+		game_man = get_tree().get_first_node_in_group("game")
+		level_man = get_tree().get_first_node_in_group("level_manager")
+		print(game_man.current_character)
+		var character = game_man.current_character
+		if character == "panko":
+			panko.visible = true
+		if character == "lumi":
+			lumi.visible = true
+		if character == "pippa":
+			pippa.visible = true
+		if character == "tenma":
+			tenma.visible = true
+		if character == "lia":
+			lia.visible = true
+		retry_button.visible = false
+		level_select_button.visible = false
+		main_menu_button.visible = false
+		continue_button.visible = true
+		#$Timer.start()
 
 func track_b():
 	AudioManager.play_music(victory_music_b)
@@ -99,5 +133,14 @@ func _on_next_level_pressed():
 	select_graphic.global_position = $CanvasLayer/MarginContainer/Control/HBoxContainer/NextLevel.global_position
 	await get_tree().create_timer(.8).timeout
 	game_man.on_settings_to_level_select()
+	AudioManager.stop_looped()
+	queue_free()
+
+
+func _on_continue_pressed() -> void:
+	select_anim.play("selected")
+	select_graphic.global_position = $CanvasLayer/MarginContainer/Control/HBoxContainer/Retry.global_position
+	await get_tree().create_timer(.8).timeout
+	# TO WIN CON END HERE
 	AudioManager.stop_looped()
 	queue_free()
